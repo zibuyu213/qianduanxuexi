8ce88932338b528c829f56e79dfe8517107dab4a
zibuyu213 <m18335835389@163.com>
2019-06-10 22:32:56 +0800

    '123'

--- src/containers/Home/Today.jsx
+++ src/containers/Home/Today.jsx
@@ -14,15 +14,6 @@ export default class Today extends Component {
         super(props);
         this.state = {
           firstdata:{
-            a:0,
-            b:0,
-            c:0,
-            d:0,
-            e:0,
-            f:0,
-            g:0,
-            h:0,
-            i:0,
           },
         data :[],
         bztdata : [],
